0bb65dad21419a212e27ca7db82f2b70e66b5388
behindname <behindname@naver.com>
2021-03-14 12:38:40 +0900

    Add enum localization to believer show and form

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def enum_ko(column_name)
+    return '없음' if self.send(column_name).nil?
+    I18n.t("enum.#{self.class.name.underscore}.#{column_name}.#{self.send("#{column_name}")}")
+  end
+
+  def self.enum_selectors(column_name)
+    I18n.t("enum.#{self.name.underscore}.#{column_name}").invert rescue []
+  end
+
 end
