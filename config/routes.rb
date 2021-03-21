ceba5fe3e0bafba79516c23b6066d7b68f7848a2
behindname <behindname@naver.com>
2021-03-21 11:17:53 +0900

    Sync to remote

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :believers
   resources :prays do
-    resources :pray_requests, only: %i[index create new]    
+    resources :pray_requests, only: %i[index create new]
   end
 
   resources :pray_requests, except: %i[index create new]
